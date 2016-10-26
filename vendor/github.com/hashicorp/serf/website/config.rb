00d66a766fd15f33e387d24b07e5870f56da3815
Max Franks <eliquious@gmail.com>
2016-10-25 22:18:59 -0500

    Update deps and vendor

---
+++ vendor/github.com/hashicorp/serf/website/config.rb
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------
+# Configure Middleman
+#-------------------------------------------------------------------------
+
+set :base_url, "https://www.serfdom.io/"
+
+activate :hashicorp do |h|
+  h.version         = "0.6.4"
+  h.bintray_enabled = ENV["BINTRAY_ENABLED"]
+  h.bintray_repo    = "mitchellh/serf"
+  h.bintray_user    = "mitchellh"
+  h.bintray_key     = ENV["BINTRAY_API_KEY"]
+
+  # Currently, Serf builds are not prefixed with serf_*
+  h.bintray_prefixed = false
+end
+
+helpers do
+  # This helps by setting the "active" class for sidebar nav elements
+  # if the YAML frontmatter matches the expected value.
+  def sidebar_current(expected)
+    current = current_page.data.sidebar_current
+    if current.start_with?(expected)
+      return " class=\"active\""
+    else
+      return ""
+    end
+  end
+end
